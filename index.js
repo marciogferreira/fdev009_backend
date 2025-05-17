2582a29e213cf14547339e22c9ffe2210f52516f
Márcio Ferreira <hmarciogferreira@gmail.com>
2025-05-17 15:12:17 -0300

    Express

--- index.js
+++ index.js
@@ -1,61 +1,15 @@
-import { createServer } from 'node:http';
-import UsuarioModel from './src/models/UsuarioModel.js'
+import express from 'express'
+import UsuarioModel from './src/models/UsuarioModel.js';
+const app = express();
+app.get('/', (request, response) => {
+    response.send("Hello Express FDEV009 111c")
+})
 
+app.get('/usuarios', async (request, response) => {
+    const usuarios = await UsuarioModel.findAll();
+    response.json(usuarios)
+})
 
-const app = createServer((request, response) => {
-
-    const { url, method }  = request;
-    // GET - http://localhost:3000/usuarios
-    if(url === '/usuarios' && method === 'get') {
-        const retorno = UsuarioModel.findAll();
-        response.writeHead(200, { 'content-type': 'text/json' })
-        response.end({ 
-            message: 'Lista de Usuario',
-            data: retorno
-        })
-    }
-
-    // POST - http://localhost:3000/usuarios
-    if(url === '/usuarios' && method === 'post') {
-        const dados = request.body;
-        const retorno = UsuarioModel.create(dados);
-        response.writeHead(200, { 'content-type': 'text/json' })
-        response.end({ 
-            message: 'Usuario Cadastrado com Sucesso',
-            data: retorno
-        })
-    }
-
-    // PUT - http://localhost:3000/usuarios/2
-    if(url === '/usuarios' && method === 'put') {
-        const dados = request.body;
-        const id = request.params.id;
-        const retorno = UsuarioModel.update(dados, { id: id });
-        response.writeHead(200, { 'content-type': 'text/json' })
-        response.end({ 
-            message: 'Usuario Atualizado com Sucesso',
-            data: retorno
-        })
-    }
-
-     // DELETE - http://localhost:3000/usuarios/2
-     if(url === '/usuarios' && method === 'delete') {
-        const id = request.params.id;
-        const retorno = UsuarioModel.destroy({ id: id });
-        response.writeHead(200, { 'content-type': 'text/json' })
-        response.end({ 
-            message: 'Usuario Deletado com Sucesso',
-            data: retorno
-        })
-    }
-
-    // CRUD DE CATEGORIAS
-    
-
-    response.writeHead(200, { 'content-type': 'text/plain' })
-    response.end('Hello NodeJs FS 009')
-});
-
-app.listen(3000, 'localhost', () => {
-    console.log("Servidor executando em http://localhost:3000")
+app.listen(3000, () => {
+    console.log("Servidor executando...")
 })
